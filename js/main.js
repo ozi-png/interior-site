0b85339c2d5dde2d4d6d51aa957e8ff30cd9b555
ozi-png <ozi.02.02.1996@mail.ru>
2025-01-05 21:32:27 +0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,17 @@
+VANTA.FOG({
+    el: "#contacts",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    midtoneColor: 0x1d1717
+  })
+  VANTA.CLOUDS({
+    el: "#services",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200.00,
+    minWidth: 200.00
+  })
